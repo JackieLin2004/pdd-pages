445a5db2d8e69b395dd83b59c1c5d6ebc982968d
JackieLin2004 <linxy20040304@163.com>
2024-07-06 10:50:17 +0800

    Optimized the size of navigation bar icons

--- src/components/Navigate.vue
+++ src/components/Navigate.vue
@@ -6,31 +6,46 @@ import {RouterLink} from "vue-router";
   <div class="navigate">
     <RouterLink :to="{name:'home'}" active-class="active">
       <div class="menu">
-        <span class="iconfont icon-shouye"></span>
+        <div class="icon-font">
+          <span class="iconfont icon-shouye"></span>
+        </div>
         <span>首页</span>
       </div>
     </RouterLink>
     <RouterLink :to="{name:'video'}" active-class="active">
       <div class="menu">
-        <span class="iconfont icon-shipin"></span>
+        <div class="icon-font">
+          <span class="iconfont icon-shipin"></span>
+        </div>
         <span>多多视频</span>
       </div>
     </RouterLink>
     <RouterLink :to="{name:'coupon'}" active-class="active">
       <div class="menu">
-        <span class="iconfont icon-icon-test"></span>
+        <div class="icon-font">
+          <strong>
+            <span style="font-size: 1.5rem;margin-right: 0.5vw">&#165</span>
+          </strong>
+          <strong>
+            <span class="iconfont icon-icon-test" style="font-size: 2.25rem;margin-left: 0.5vw"></span>
+          </strong>
+        </div>
         <span>暑假优惠券</span>
       </div>
     </RouterLink>
     <RouterLink :to="{name:'chat'}" active-class="active">
       <div class="menu">
-        <span class="iconfont icon-bg-chat"></span>
+        <div class="icon-font">
+          <span class="iconfont icon-bg-chat"></span>
+        </div>
         <span>聊天</span>
       </div>
     </RouterLink>
     <RouterLink :to="{name:'person'}" active-class="active">
       <div class="menu">
-        <span class="iconfont icon-gerenzhongxin"></span>
+        <div class="icon-font">
+          <span class="iconfont icon-gerenzhongxin" style="font-size: 2.25rem"></span>
+        </div>
         <span>个人中心</span>
       </div>
     </RouterLink>
@@ -62,7 +77,7 @@ import {RouterLink} from "vue-router";
   cursor: pointer;
 
   font-weight: 500;
-  font-size: 1rem;
+  font-size: 0.9rem;
 }
 
 .navigate a.active {
@@ -76,7 +91,15 @@ import {RouterLink} from "vue-router";
   align-items: center;
 }
 
-.navigate .menu .iconfont {
+.navigate .menu .icon-font {
+  width: 8vw;
+  height: 8vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.navigate .menu .icon-font .iconfont {
   font-size: 1.8rem;
 }
 </style>
